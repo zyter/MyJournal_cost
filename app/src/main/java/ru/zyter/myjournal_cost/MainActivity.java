a03b3d86fd08959fc29364b17f7fcd241a167f39
zyter <q1112230@gmail.com>
2016-01-01 18:48:21 +0700

    Created MainActivity class and layout

---
+++ app/src/main/java/ru/zyter/myjournal_cost/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.zyter.myjournal_cost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Андрей on 01.01.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiti_main);
+    }
+
+}
