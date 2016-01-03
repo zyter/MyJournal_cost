38f059a3507ff64dc7a1cb8d1476495b674eaee1
zyter <q1112230@gmail.com>
2016-01-04 01:55:42 +0700

    Start to implementing NavigationView.

--- app/src/main/java/ru/zyter/myjournal_cost/MainActivity.java
+++ app/src/main/java/ru/zyter/myjournal_cost/MainActivity.java
@@ -2,15 +2,17 @@ package ru.zyter.myjournal_cost;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiti_main);
 
