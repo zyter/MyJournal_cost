57915f9114aaa0e38d8f1840513de03b8354a2c3
zyter <q1112230@gmail.com>
2016-01-03 21:27:36 +0700

    Added Toolbar and search button

--- app/src/main/java/ru/zyter/myjournal_cost/MainActivity.java
+++ app/src/main/java/ru/zyter/myjournal_cost/MainActivity.java
@@ -2,16 +2,32 @@ package ru.zyter.myjournal_cost;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Андрей on 01.01.2016.
- */
 public class MainActivity extends Activity {
+
+   private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiti_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 
 }
